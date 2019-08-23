c056adb8d7b08269ffc342ce339ed363c91f8996
lhb-web <53331831+lhb-web@users.noreply.github.com>
2019-08-23 17:58:16 +0800

    上课笔记

---
+++ 前端代码/专题-node/demo_readFile.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+fs.readFile('tempfile.txt', function(err, data) {
+	if (err) {
+		return console.error(err);
+	} else {
+		console.log(data.toString());
+	}
+});
+console.log("程序执行结束！，留意本行和输出的时序");
