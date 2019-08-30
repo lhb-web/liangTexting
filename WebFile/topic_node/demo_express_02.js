b9c33a720b6839c6695e11dafedd63bdfa2b62fd
lhb-web <53331831+lhb-web@users.noreply.github.com>
2019-08-30 18:00:25 +0800

    服务器get 和 post

--- WebFile/topic_node/demo_express_02.js
+++ WebFile/topic_node/demo_express_02.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
+var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({
+	extended: false
+});
 
-app.use('/public', express.static('public'));
+const connection = mysql.createConnection({
+	host: "localhost", // 主机地址
+	port: 3306, // 如果修改了数据库的默认端口，这个属性不可忽略
+	user: "root", // 数据库用户名
+	password: "", // 数据库用户密码
+	database: "liang" // 数据库名
+});
+
+connection.connect();
+
+app.use('/web', express.static('web'));// 将/web文件夹下的文件当作静态资源,不需要额外的get去获取
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
@@ -20,7 +34,8 @@ app.get('/', function(req, res) {
 app.post('/file_upload', function(req, res) {
 
 	console.log(req.files[0]); // 上传的文件信息
-
+	
+	
 	var des_file = __dirname + "/" + req.files[0].originalname;
 	fs.readFile(req.files[0].path, function(err, data) {
 		fs.writeFile(des_file, data, function(err) {
@@ -37,6 +52,36 @@ app.post('/file_upload', function(req, res) {
 		});
 	});
 })
+
+app.get("/getUserInfo",function(req, res){
+	console.info(req.query);
+	connection.query('select * from Person where id="'+req.query.userId+'" limit 1',function(err,result,file){
+		if(err){
+			console.info(err);
+			console.info("查无此人")
+		}else{
+			console.info(result);
+			if(result.length>0){
+				res.send(result[0].name);
+			}else{
+				res.send("查无此人");
+			}
+		}
+	});
+});
+
+app.post("/updateUser",urlencodedParser,function(req,res){
+	console.info(req.body);
+	connection.query('update Person set name="' +req.body.userName + '" where id=' +req.body.userId, function(error, results,fields) {
+		if (error) {
+			throw error;
+		}
+	});
+	response.writeHead(200, {
+		'Content-Type': 'text/plain;charset=utf-8'
+	});
+	response.end('修改成功!');
+});
 
 var server = app.listen(8081, function() {
 
